10de65d5ef0a9c67d0e106b60fd6f450ca85700f
Brandon Bennett <bennetb@gmail.com>
2012-01-05 23:48:31 -0700

    Add lcd.  Use state machines.
    This adds code for a 16x2 LCD screen to provide further information
    about how long the dog has been fed.
    
    Also reminds us that Hank, in fact, is fat (like us) when you hit
    the reset button.
    
    Also major rewrite to incoportate state machines for LCD, LED, and
    LID state along with other code clean ups.

--- HankyHankyFattyFatty.ino
+++ HankyHankyFattyFatty.ino
@@ -1,17 +1,57 @@
 /* 
  * Hanky Hanky Fatty Fatty
  */
+#include <SoftwareSerial.h>
+#include <serLCD.h>
  
-int PIN_LED = 3;
-int PIN_SWITCH_RESET = 4;
-int PIN_SWITCH_LID = 5;
-int PIN_LED_INTERNAL = 13;  
+#define PIN_LED          3
+#define PIN_SWITCH_RESET 4
+#define PIN_SWITCH_LID   5
+#define PIN_LCD_SERIAL   6
+#define PIN_LED_INTERNAL 13
 
-int TICK = 100;
-unsigned long REMINDER_TIMEOUT = (60 * 60 * 2); // 2 hours
+#define TICK 20
+#define REMINDER_TIMEOUT (60 * 60 * 2)
 
-long time_end = 0;
-boolean fed_state = false;
+serLCD lcd(PIN_LCD_SERIAL);
+
+long time_fed = secs();
+
+const char *lcd_text[][2] PROGMEM = {
+//  1234567890123456  1234567890123456
+  {"      Init",      ""               },
+  {"    Lid Open!",   ""               },
+  {"H was last fed",  "      hours agp"},
+  {"Hanky Hanky",     "    Fatty Fatty"}
+};
+
+enum lcd_states {
+  LCD_INIT = 0,
+  LCD_LID_OPEN = 1,
+  LCD_LAST_FED = 2,
+  LCD_FATTY_FATTY = 3
+};
+
+lcd_states lcd_state = LCD_LID_OPEN;
+lcd_states prev_lcd_state;
+long last_lcd_state_change = secs();
+
+enum lid_states {
+  LID_OPEN,
+  LID_CLOSED
+};
+
+lid_states lid_state = LID_OPEN;
+lid_states prev_lid_state;
+
+enum led_states {
+  LED_OFF,
+  LED_ON,
+  LED_PULSE
+};
+
+led_states led_state = LED_OFF;
+led_states prev_led_state;
 
 int led_brightness = 0;
 int led_fade_ammount = 10;
@@ -23,45 +63,119 @@ void setup() {
 }
 
 void loop() {
-  // Main loop
-  update_led(fed_state);
-  
-  if (fed_state) {
-    /* Check to see if we need to reset the light based on time */
-    if (time_end - ((long)millis()/1000) <= 0 ) {
-      fed_state = false;
-    }
-      
-    /* Check to see if anyone pushed the reset button */
-    if (digitalRead(PIN_SWITCH_RESET) == HIGH) {
-      fed_state = false;
-    }
-  } 
-    
-  if (!fed_state && digitalRead(PIN_SWITCH_LID) == HIGH) {
-    time_end = ((long)millis()/1000) + REMINDER_TIMEOUT;
-    fed_state = true;
-  }
+  update_lid_state();
+  update_led();
+  update_lcd();
   
   delay(TICK);
 }
 
-void update_led(int state) {
-  if (state) { 
-    analogWrite(PIN_LED, led_brightness);
-    
-    led_brightness = led_brightness + led_fade_ammount;
-    
-    if (led_brightness > 255) 
-      led_brightness = 255;
-    else if (led_brightness < 0)
-      led_brightness = 0;
-    
-    if (led_brightness <= 0 || led_brightness >= 255)
-       led_fade_ammount = -led_fade_ammount;
+// Seconds since started
+long secs() {
+  return (long)millis() / 1000;
+}
 
+void update_lid_state() {
+  prev_lid_state = lid_state;
+
+  if (digitalRead(PIN_SWITCH_LID) == LOW) {
+    /* Lid is open */
+    lid_state = LID_OPEN;
   } else {
-    analogWrite(PIN_LED, 0);
-    led_brightness = 0;
+    lid_state = LID_CLOSED;
   }
+
+  switch(lid_state) {
+    case LID_OPEN:
+       if (prev_lid_state != LID_OPEN) {
+         led_state = LED_ON;
+         lcd_state = LCD_LID_OPEN;
+         time_fed = secs();
+       }
+       break;
+
+    case LID_CLOSED:
+       if (prev_lid_state != LID_CLOSED) {
+         lcd_state = LCD_LAST_FED;
+         led_state = LED_PULSE;
+       }
+
+       if (led_state != LED_OFF && time_fed + REMINDER_TIMEOUT - secs() <= 0) {
+         led_state = LED_OFF;
+       }
+
+       if (digitalRead(PIN_SWITCH_RESET) == HIGH) {
+         if (led_state != LED_OFF) {
+           led_state = LED_OFF;
+         }
+         lcd_state = LCD_FATTY_FATTY;
+       }
+       break;
+  }
+}
+
+void update_lcd() {
+  lcd_states orig_lcd_state = lcd_state;
+
+  if (prev_lcd_state != lcd_state) {
+    lcd.clear();
+    lcd.print(lcd_text[lcd_state][0]);
+    lcd.selectLine(2);
+    lcd.print(lcd_text[lcd_state][1]);
+
+    last_lcd_state_change = secs();
+  }
+
+  float hours_since_fed = (secs() - time_fed) / 60.0 / 60.0;
+
+  switch (lcd_state) {
+    case LCD_LAST_FED:
+      lcd.setCursor(2, 1);
+      lcd.print(hours_since_fed, 2);
+      break;
+
+    case LCD_FATTY_FATTY:
+      //Reset back to LAST_FED after 5 seconds
+      if ((last_lcd_state_change + 5) - secs() <= 0) {
+        lcd_state = LCD_LAST_FED;
+      }
+      break;
+  }
+
+  prev_lcd_state = orig_lcd_state;
+}
+
+
+void update_led() {
+  
+  switch (led_state) {
+    case LED_OFF:
+      if (prev_led_state != LED_OFF)
+        analogWrite(PIN_LED, 0);
+      break;
+
+    case LED_ON:
+      if (prev_led_state != LED_ON)
+        analogWrite(PIN_LED, 255);
+      break;
+
+    case LED_PULSE:
+      if (prev_led_state != LED_PULSE)
+        led_brightness = 0;
+
+      analogWrite(PIN_LED, led_brightness);
+
+      led_brightness = led_brightness + led_fade_ammount;
+
+      if (led_brightness > 255)
+        led_brightness = 255;
+      else if (led_brightness < 0)
+        led_brightness = 0;
+
+      if (led_brightness <= 0 || led_brightness >= 255)
+         led_fade_ammount = -led_fade_ammount;
+
+      break;
+  }
+  prev_led_state = led_state
 }
